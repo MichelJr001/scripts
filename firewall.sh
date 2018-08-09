49e78c43a4b96aa625a68089f9fbc21ae02dbd29
root <root@Server-01.qyart.com>
2018-08-09 16:19:24 -0300

    My basic firewall

---
+++ firewall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Limpar regras existentes
+
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+
+# Evita DoS (Denial of Service)
+echo 1 > /proc/sys/net/ipv4/tcp_syncookies # Ativa
+# echo 0 > /proc/sys/net/ipv4/tcp_syncookies # Desativa
+
+# Libera portas necessarias
+
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT # SSH
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT # HTTP
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT # HTTPS
+iptables -A INPUT -p tcp --dport 3306 -j ACCEPT # MYSQL
+iptables -A INPUT -p ICMP -j DROP # PING
+# Bloqueio de demais portar
+
+iptables -A INPUT -p tcp --syn -j DROP
