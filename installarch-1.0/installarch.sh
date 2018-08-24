acccaf7266b79fbe9d2017420fe544a16c3ee33e
MichelJr001 <michelcontato001@outlook.com>
2018-08-24 14:22:06 -0300

    v1.0

---
+++ installarch-1.0/installarch.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+# @author: Michel Anderson
+# @Github: MichelJr001
+# @Twitter: _Michel_Jr_
+
+clear
+
+# Cores a serem usadas no script
+corPadrao="\033[0m"
+vermelho="\033[31m"
+verde="\033[32m"
+amarelo="\033[33m"
+
+
+
+echo -e """
+$vermelho
+\t\t _____           _        _ _                  _     
+\t\t|_   _|         | |      | | |                | |$verde v1.0 $vermelho 
+\t\t  | |  _ __  ___| |_ __ _| | |   __ _ _ __ ___| |__  
+\t\t  | | | '_ \/ __| __/ _  | | |  / _  | '__/ __| '_ \ 
+\t\t _| |_| | | \__ \ || (_| | | | | (_| | | | (__| | | |
+\t\t|_____|_| |_|___/\__\__,_|_|_|  \__,_|_|  \___|_| |_|\n
+
+ $verde Github:$corPadrao https://github.com/MichelJr001
+ $verde twitter:$corPadrao https://twitter.com/_Michel_Jr_
+
+
+Iniciando instalação...\n"""
+
+# Muda o layout do teclado
+echo -e "[$verde installarch $corPadrao]: Mudando layout do teclado..."
+# loadkeys br-abnt2
+echo -e "[$verde OK $corPadrao]"
+
+# Inicia o particionador de hd
+echo -e "[$verde installarch $corPadrao]: Iniciando particionador de HD..."
+# cfdisk
+echo -e "[$verde OK $corPadrao]"
+
+# Monta as partições criadas
+echo -e "[$verde installarch $corPadrao]: Montando partições..."
+# mkfs.ext4 /dev/sda1
+# mkswap /dev/sda2
+# swapon /dev/sda2
+# lsblk
+echo -e "[$verde OK $corPadrao]"
+
+# Instala o sistema basico nas partições montadas
+echo -e "[$verde installarch $corPadrao]: Instalando o sistema basico..."
+# pacstrap -i /mnt base base-devel
+# genfstab -U -p /mnt >> /mnt/etc/fstab
+echo -e "[$verde OK $corPadrao]"
+
+# Configura o sistema basico
+echo -e "[$verde installarch $corPadrao]: Configurando o sistema basico..."
+# arch-chroot /mnt
+# locale-gen
+# LANG="pt_PT.UTF-8" locale > /etc/locale.conf
+# echo "KEYMAP=pt-latin9" > /etc/vconsole.conf
+# ln -s /usr/share/zoneinfo/Europe/Lisbon /etc/localtime
+# mkinitcpio -p linux
+echo -e "[$verde OK $corPadrao]"
+
+# Instala o inicializador GRUB
+echo -e "[$verde installarch $corPadrao]: Inicinado instalação do GRUB..."
+# pacman -S grub
+# grub-install /dev/sda
+# pacman -S os-prober
+# grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "[$verde OK $corPadrao]"
+
+# Configura e adiciona mais usuarios
+echo -e "[$verde installarch $corPadrao]: Configurando usuarios..."
+echo -e "[$verde installarch $corPadrao]: Senha de root..."
+# passwd
+echo -e "[$verde installarch $corPadrao]: Novo usuario..."
+# read -p "[ nome ]: " nome
+# read -p "[$amarelo nome   $corPadrao ]: " nome
+# useradd -m $nome
+# passwd $nome 
+echo -e "[$verde OK $corPadrao]"
+
+# Desmonta a partição e reinicia o sistema
+echo -e "[$verde installarch $corPadrao]: Finalizando instalação..."
+# umount /mnt
+# clear
+
+read -p "Deseja instalar uma interface grafica? [S/N]: " resp; 
+echo $resp
+
+if [$resp == N]; then
+    echo -e """ 
+    \t\t Menu de interfaces
+    \t\t ---- -- ----------
+
+    \t 1) Gnome
+    \t 2) KDE
+    \t 3) Xfce
+    \t 4) Lxde
+    """
+else
+    echo -e "[ $verdeOK   $corPadrao]"
+fi
+
+echo -e "$vermelho
+\t\t _____           _        _ _                  _     
+\t\t|_   _|         | |      | | |                | |$verde v1.0$vermelho 
+\t\t  | |  _ __  ___| |_ __ _| | |   __ _ _ __ ___| |__  
+\t\t  | | | '_ \/ __| __/ _  | | |  / _  | '__/ __| '_ \ 
+\t\t _| |_| | | \__ \ || (_| | | | | (_| | | | (__| | | |
+\t\t|_____|_| |_|___/\__\__,_|_|_|  \__,_|_|  \___|_| |_|\n
+
+ $verde Github:$corPadrao https://github.com/MichelJr001
+ $verde twitter:$corPadrao https://twitter.com/_Michel_Jr_
+
+ Retire o disco de instalação
+"
+sleep 3
+# reboot
